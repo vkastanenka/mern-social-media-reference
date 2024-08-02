1e9807f1c44c0e247a41bdba33d7b8a41829bf48
Victoria Kastanenka <vkastanenka@gmail.com>
2024-08-02 14:07:12 -0400

    added jwt auth

--- server/index.js
+++ server/index.js
@@ -38,7 +38,6 @@ const upload = multer({ storage })
 
 /* ROUTES WITH FILES */
 app.post('/auth/register', upload.single('picture'), register)
-// app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use('/auth', authRoutes)
